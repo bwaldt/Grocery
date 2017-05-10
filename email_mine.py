27978db5892edfcc98cf4c4f5341aa4ada459717
bwaldt <waldt@rutgers.edu>
2017-05-10 18:20:55 -0400

    Parse string to only include grocery list

--- email_mine.py
+++ email_mine.py
@@ -2,6 +2,7 @@ import smtplib
 import time
 import imaplib
 import email
+import re
 
 ORG_EMAIL = "@gmail.com"
 FROM_EMAIL = "trackmyfood123" + ORG_EMAIL
@@ -20,18 +21,32 @@ id_list = mail_ids.split()
 first_email_id = int(id_list[0])
 latest_email_id = int(id_list[-1])
 
-for i in range(latest_email_id, first_email_id, -1):
-    typ, data = mail.fetch(i, '(RFC822)')
+def get_acme():
+    for i in range(latest_email_id, first_email_id, -1):
+        typ, data = mail.fetch(i, '(RFC822)')
 
-    for response_part in data:
-        if isinstance(response_part, tuple):
-            msg = email.message_from_string(response_part[1])
-            email_subject = msg['subject']
-            email_from = msg['from']
-            if 'Receipt' in email_subject:
-                if msg.is_multipart():
-                    for part in msg.walk():
-                        body = part.get_payload()
-                        print body
+        for response_part in data:
+            if isinstance(response_part, tuple):
+                msg = email.message_from_string(response_part[1])
+                email_subject = msg['subject']
+                email_from = msg['from']
+                if 'Receipt' in email_subject:
+                    if msg.is_multipart():
+                        for part in msg.walk():
+                            body = part.get_payload()
+                            rest = body[0]
+                            return str(rest)
 
-print "I am Here"
+
+groceryList = get_acme()
+
+import string
+groceryList = groceryList.translate(None,string.ascii_lowercase)
+#print groceryList
+
+
+test ='this is text to keep T N: 2912017050720040050290 [: M ] R F Y <#_-3746339460747013820__3961495309290320556_> $5.00  $50.00'
+
+groceryList = groceryList.split("[: M")[0]
+groceryList = groceryList.split("//>")[1]
+print groceryList
